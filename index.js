395c075ca96174ae168dec507ff13953143b0d94
Euclides Vasconcelos <evascon@motorola.com>
2024-11-18 18:33:15 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  const name = process.env.NAME || 'world';
+  res.send(`Hello ${name}!`);
+});
+
+const port = parseInt(process.env.PORT) || 8081;
+app.listen(port, () => {
+  console.log(`helloworld: listening on port ${port}`);
+});
